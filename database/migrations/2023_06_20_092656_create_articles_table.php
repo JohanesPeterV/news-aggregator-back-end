2e087cbe2cd26212682c7eb81ea219f046d98a65
JopHme <60497959+JopHme@users.noreply.github.com>
2023-06-23 18:46:12 +0700

    chores: create article model and controller

--- database/migrations/2023_06_20_092656_create_articles_table.php
+++ database/migrations/2023_06_20_092656_create_articles_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('title');
             $table->string('description');
             $table->string('author');
-            $table->string('author');
+
             $table->string('url');
             $table->string('source');
             $table->date('publish_date');
